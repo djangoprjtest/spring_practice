b90ffbbe81a9de46de4ebb3ba79de13f54b85c20
djangotestprj <flqjemfkq@naver.com>
2021-09-30 17:52:08 +0900

    9월 30일

--- prj_rest/src/main/java/org/ict/controller/TestController.java
+++ prj_rest/src/main/java/org/ict/controller/TestController.java
@@ -1,5 +1,14 @@
 package org.ict.controller;
 
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.ict.domain.TestVO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +16,89 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
+	@RequestMapping("/hello")
+	public String sayHello() {
+		return "Hello Hello";
+	}
+	
+	@RequestMapping("/sendVO")
+	public TestVO sendTestVO() {
+		TestVO testVO = new TestVO();
+		
+		testVO.setName("채종훈");
+		testVO.setAge(21);
+		testVO.setMno(1);
+		return testVO;
+	}
+	
+	@RequestMapping("/sendVOList")
+	public List<TestVO> sendVOList(){
+		List<TestVO> list = new ArrayList<TestVO>();
+		for(int i=0; i<10; i++) {
+			TestVO vo = new TestVO();
+			vo.setMno(i);
+			vo.setName(i + "종훈");
+			vo.setAge(20 + i);
+			list.add(vo);
+		}
+		return list;
+	}
+	
+	@RequestMapping("/sendMap")
+	public Map<Integer, TestVO> sendMap(){
+		
+		Map<Integer, TestVO> map = new HashMap<Integer, TestVO>();
+		
+		for(int i =0; i<10; i++) {
+			TestVO vo = new TestVO();
+			vo.setName("채종훈");
+			vo.setMno(i);
+			vo.setAge(50 + i);
+			map.put(i,  vo);
+			
+		}
+		// map의 키값(왼쪽에 선언한것)은 중복된 값이 들어올 수 없고
+		// 들어온다면 가장 마지막에 넣은 하나만 남습니다.
+		TestVO vo = new TestVO();
+		vo.setName("가나다");
+		vo.setMno(50);
+		vo.setAge(1000);
+		map.put(0, vo);
+		System.out.println(map);
+		return map;
+	}
+	
+	@RequestMapping("/sendErrorAuth")
+	public ResponseEntity<Void> sendListAuth(){
+		
+		// ResponseEntity는 생성자에 HttpStatus.코드번호
+		// 를 적어서 해당 주소 접속시 어떤 접속코드를 사용자에게 넘겨줄지
+		// 결정할 수 있습니다.
+		return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+			
+	}
+	
+	@RequestMapping("/sendErrorNot")
+	public ResponseEntity<List<TestVO>> sendListNot(){
+		
+		List<TestVO> list = new ArrayList<TestVO>();
+		
+		for(int i = 0; i < 10; i++) {
+			TestVO vo  = new TestVO();
+			vo.setMno(i);
+			vo.setName(i + "종훈");
+			vo.setAge(20 + i);
+			list.add(vo);
+		}
+		// ResponseEntity의 생성자에, 파라미터 2개를 넘기면
+		// 전송할 데이터와, 전송시 결과로 나올 코드를 함께 넘길수 있습니다.
+		return new ResponseEntity<List<TestVO>>(
+				list, HttpStatus.NOT_FOUND);
+		
+	}
+	
+	
+	
+	
+	
 }
